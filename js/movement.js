c45c0c03c56bc79b9df0c0d4ba19ce25bfa483a1
hugocowan <hugocowan915@gmail.com>
2019-02-06 22:38:04 +0000

    Decided I went a little too crazy with the files. Merged playerCSS and movement into one file

--- js/movement.js
+++ js/movement.js
@@ -1,18 +1,14 @@
-// Change player1/2's CSS based on their direction.
+//Player movement
+function playerMovementCSS(keyCode1, keyCode2, player) {
 
-function playerDirection(keyCode1, keyCode2, player) {
-
-    if (keypress[keyCode1] && keyCode1 === 'a') {
-        playerDirectionCSS('left', player);
-    } else if (keypress[keyCode2] && keyCode2 === 'd') {
-        playerDirectionCSS('right', player);
-    } else if (keypress[keyCode1] && keyCode1 === 'ArrowLeft') {
-        playerDirectionCSS('left', player);
-    } else if (keypress[keyCode2] && keyCode2 === 'ArrowRight') {
-        playerDirectionCSS('right', player);
-    }
-
-    return player.speed;
+    player.$body.css({
+        left: function (index, oldPosition) {
+            return newPositionX(oldPosition, keyCode1, keyCode2, player);
+        },
+        top: function (index, oldPosition) {
+            return newPositionY(oldPosition, player);
+        }
+    });
 }
 
 
@@ -46,4 +42,51 @@ function newPositionY(oldPosition, player) {
 
         return newPositionY;
     }
+}
+
+
+// Change player's visual direction
+function playerDirection(keyCode1, keyCode2, player) {
+
+    if (keypress[keyCode1] && keyCode1 === 'a') {
+        playerDirectionCSS('left', player);
+    } else if (keypress[keyCode2] && keyCode2 === 'd') {
+        playerDirectionCSS('right', player);
+    } else if (keypress[keyCode1] && keyCode1 === 'ArrowLeft') {
+        playerDirectionCSS('left', player);
+    } else if (keypress[keyCode2] && keyCode2 === 'ArrowRight') {
+        playerDirectionCSS('right', player);
+    }
+
+    return player.speed;
+}
+
+
+//Change player's visual direction
+function playerDirectionCSS(direction, player) {
+
+    let playerCSS = { borderRadius: '15px 2px 0 0' },
+        visorCSS = { left: '0px', borderRadius: '10px 2px 0 0' },
+        gunCSS = { left: '-9px' };
+
+    if (direction === 'right') {
+        playerCSS = { borderRadius: '2px 15px 0 0' };
+        visorCSS = { left: '3.6px', borderRadius: '2px 10px 0 0' };
+        gunCSS = { left: '20px' };
+    }
+
+    player.direction = direction;
+    player.$body.css(playerCSS);
+    player.$visor.css(visorCSS);
+    player.$gun.css(gunCSS);
+}
+
+
+//Jumping function
+function characterJump(player) {
+
+    player.airborne = true;
+    player.$body.animate({
+        'top': `-=${player.jumpHeight}px`
+    });
 }
